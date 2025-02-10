eb4236a4d5b0209bea2c029cac77272c8e52e5ff
fahmi-nugroho <fahmingroho23@gmail.com>
2025-02-10 15:56:48 +0700

    feat: setup next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,43 @@
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
-import {NextUIProvider} from "@nextui-org/react";
-import {Inter} from "next/font/google";
-import {cn} from "@/utils/cn";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from "next/font/google";
+import { cn } from "@/utils/cn";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            retry: false
-        }
-    }
-})
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
-export default function App({Component, pageProps}: AppProps) {
-    return (
-        <QueryClientProvider client={queryClient}>
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
         <NextUIProvider>
-            <main className={cn(inter.className, "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0")}>
-                <Component {...pageProps} />
-            </main>
+          <main
+            className={cn(
+              inter.className,
+              "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0"
+            )}
+          >
+            <Component {...pageProps} />
+          </main>
         </NextUIProvider>
-        </QueryClientProvider>
-    );
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
---
+++ src/pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,80 @@
+import environtments from "@/config/environtments";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { JWTExtendeded, SessionExtended, UserExtended } from "@/types/Auth";
+import authServices from "@/services/auth.services";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24,
+  },
+  secret: environtments.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        identifier: { label: "identifier", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"identifier" | "password", string> | undefined
+      ): Promise<UserExtended | null> {
+        const { identifier, password } = credentials as {
+          identifier: string;
+          password: string;
+        };
+
+        const result = await authServices.login({
+          identifier,
+          password,
+        });
+
+        const accesToken = result.data.data;
+
+        const me = await authServices.getProfileWithToken(accesToken);
+        const user = me.data.data;
+
+        if (
+          accesToken &&
+          result.status === 200 &&
+          user._id &&
+          me.status === 200
+        ) {
+          user.accesToken = accesToken;
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({
+      token,
+      user,
+    }: {
+      token: JWTExtendeded;
+      user: UserExtended | null;
+    }) {
+      if (user) {
+        token.user = user;
+      }
+
+      return token;
+    },
+    async session({
+      session,
+      token,
+    }: {
+      session: SessionExtended;
+      token: JWTExtendeded;
+    }) {
+      session.user = token.user;
+      session.accesToken = token.user?.accesToken;
+
+      return session;
+    },
+  },
+});
